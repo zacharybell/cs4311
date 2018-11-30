f989403bdcfbef634e575623d9b54e867f285de6
Victor Luna <lunavictor068@gmail.com>
2018-11-30 15:12:56 -0700

    Update StateMachine and add Transition

--- app/state_machine.py
+++ app/state_machine.py
@@ -1,28 +1,22 @@
 import networkx as nx
-import matplotlib as mpl
-mpl.use('TkAgg')
-import matplotlib.pyplot as plt
-import pydot
 from networkx.drawing.nx_pydot import write_dot
 
-class state_machine(DiGraph):
-
-	__init__(self, name):
-		self.name = name
-		self.graph = nx.DiGraph()
+class StateMachine(nx.DiGraph):
+	
+	def __init__(self, name):
+		self._name = name
+		
 
 	def generate(self):
-		write_dot(g, "graph.dot")
+		write_dot(self, "graph.dot")
 
-# g = nx.DiGraph()
-# g.add_nodes_from(["type 1", "type 2"])
-# g.add_edge("type 1","type 2")
-# options = {
-#      'node_color': 'blue',
-#      'node_size': 100,
-#      'width': 3,
-#      'arrowstyle': '-|>',
-#      'arrowsize': 12,
-# }
-# nx.draw_networkx(g, arrows=True, **options)
-# write_dot(g, "dot.dot")
+
+class Transition:
+
+	def __init__(self, source, target):
+		self._source = source
+		self._target = target
+
+	def edit(source, target):
+		self._source = source
+		self._target = target
