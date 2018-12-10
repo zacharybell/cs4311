d796708a714d578e23352195c86fd33db7be0b5b
Zachary Bell <zacharybell@live.com>
2018-12-09 21:58:59 -0700

    Remove XML unmarshalling from the external module
    This feature is now in the Pdml module.

--- ntbsg/external.py
+++ ntbsg/external.py
@@ -18,28 +18,3 @@ def convert_pcap_to_pdml(src: str, dest: str) -> int:
 
     args = f'tshark -r {src} -T pdml > {dest}'
     return subprocess.call(args, shell=True)
-
-
-def parse_pdml(file: str) -> Pdml:
-    
-    pdml_tree = ElementTree.parse(file)
-
-    pdml_element = pdml_tree.getroot()
-
-    # if pdml_element.tag == 'pdml':
-    #     root = Pdml._convert_element_to_pdml(pdml_element)
-        
-    #     packet_elements = pdml_element.getchildren()
-
-    children = pdml_element.getchildren()[0].getchildren()[0].getchildren()
-
-    print('end')
-
-    return None
-
-    # print(root)
-
-    # return root
-
-
-parse_pdml('samples/cubic.pdml')
