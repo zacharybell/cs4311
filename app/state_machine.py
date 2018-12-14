0fe7a22f09de59275b1d4186767c46045f51572a
Victor Luna <lunavictor068@gmail.com>
2018-12-13 22:06:15 -0700

    finish state machine model

--- app/state_machine.py
+++ app/state_machine.py
@@ -1,22 +1,27 @@
 import networkx as nx
-from networkx.drawing.nx_pydot import write_dot
+from networkx.drawing.nx_agraph import graphviz_layout, to_agraph
+import pygraphviz as pgv
 
-class StateMachine(nx.DiGraph):
-	
-	def __init__(self, name):
-		self._name = name
-		
+
+class StateMachine:
+
+	def __init__(self):
+		self.graph = nx.nx.DiGraph()
+		self.graph.graph['graph'] = {'rankdir':'TD'}
+		self.graph.graph['node'] = {'shape':'circle'}
+		self.graph.graph['edges'] = {'arrowsize':'4.0'}
+
+	def add_node(self, node):
+		self.graph.add_node(node)
+
+	def reset(self, transitions):
+		self.graph.clear()
+		self.graph.add_edges_from(transitions)
+
+	def get_edges(self):
+		return self.graph.edges()
 
 	def generate(self):
-		write_dot(self, "graph.dot")
-
-
-class Transition:
-
-	def __init__(self, source, target):
-		self._source = source
-		self._target = target
-
-	def edit(source, target):
-		self._source = source
-		self._target = target
+		A = to_agraph(self.graph)
+		A.layout('dot')
+		A.draw('dottest123.png')
