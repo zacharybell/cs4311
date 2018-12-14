2fef12533215f90f346665e00ff1f6e2e1e73aba
root <root@localhost.localdomain>
2018-12-13 22:13:31 -0500

    added messageTemplate basics

---
+++ app/messageTemplate.py
@@ -0,0 +1,22 @@
+
+
+
+class messageTemplate():
+
+    def __init__(messageTypes, fieldEquivalenceDependency, fieldLengthDependency, packetLengthDependency, checksum):
+
+        self.messageTemplateName
+        self.destinationFolderName
+        self.destinationFolderPath
+        self.outputFormat
+        self.messageTypes = messageTypes
+        self.fieldEquivalenceDependeny = fieldEquivalenceDependency
+        self.fieldLengthDependency = fieldLengthDependency
+        self.packeLengthDependency = packetLengthDependency
+        self.checksum = checksum
+
+
+
+    def generateScapyFile():
+        
+    
